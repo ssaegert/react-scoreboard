77a7fb6b29600480bb47dfd6a5ee976a59e3d552
ssaegert <ssaegert@gmail.com>
2022-02-17 20:28:09 -0600

    Cleaned up code in Header JS

--- scoreboard/src/components/Header.js
+++ scoreboard/src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from 'prop-types';
 import Stats from "./Stats";
 import Stopwatch from "./Stopwatch";
-
 const Header = ({ players, title}) => {
     return (
       <header>
@@ -12,14 +11,11 @@ const Header = ({ players, title}) => {
       </header>
     );
   }
-
   Header.PropTypes = {
     title: PropTypes.string,
     players: PropTypes.arrayOf(PropTypes.object)
   };
-
   Header.defaultProps = {
     title: 'Scoreboard'
   };
-
   export default Header;
